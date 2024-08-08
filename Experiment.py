0bc4af3425adf3a54d312830a17618fb5c46f350
jeanettepep <jeanette.perschau@st.ovgu.de>
2024-08-08 12:28:57 +0200

    Update Experiment.py

--- Experiment.py
+++ Experiment.py
@@ -74,7 +74,7 @@ event.waitKeys(maxWait=30.0, keyList=["space"])
 
 instruct_stim = visual.TextStim(win) 
 instruct_stim.setText(
-"Das Experimenten besteht aus x Blöcken mit y trials. \n\n" \
+"Das Experimenten besteht aus 8 Blöcken mit 45 Trials. \n\n" \
 "Drücken Sie bitte die Taste (a) wenn ein Menschengesicht da ist und (l) wenn kein Menschengesicht da ist. \n\n" \
 "Legen Sie nun bitte die Finger auf die entsprechenden Tasten. \n\n"\
 "Drücken Sie die Leertaste für [weiter].")
