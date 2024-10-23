f1261f898f6bb2357ef96809a9abe3e20a58610c
annabell-graf <114918561+annabell-graf@users.noreply.github.com>
2024-10-23 17:21:05 +0200

    Update Experiment.py
    Leertaste kann in Trial nicht mehr gedrückt werden
    Wenn Esc gedrückt wird, muss man erst bestätigen, dass beendet wird + Daten werden gespeichter

--- Experiment.py
+++ Experiment.py
@@ -7,33 +7,22 @@ import time
 import pandas as pd
 
 # Bilder importieren
-
 img_dir     = os.path.join(os.getcwd(), "images")             # Verzeichnis anpassen
 img_all     = glob.glob(os.path.join(img_dir, "*.png"))
 img_human   = glob.glob(os.path.join(img_dir, "Mensch*"))
 img_primate = glob.glob(os.path.join(img_dir, "Affe*"))
 
-
-
-
-
-
 # Daten aufnehmen
 vp_info = {
     "age"   : [],
-    "gender": ["---","m", "w", "d"],            # vorauswählbare Antworten
+    "gender": ["---","m", "w", "d"],       
     "vp_id" : []
 }
-
 vp_data = gui.DlgFromDict(vp_info, title = "Probandendaten",                    # Titel der Dialogbox
-                          labels = ["Alter", "Geschlecht", "Probanden-ID"])                                   # Dialogbox anzeigen
-
-print(vp_info)                                                                  # Antworten werden automatisch in dict übernommen
-
-age = vp_info["age"]
+                          labels = ["Alter", "Geschlecht", "Probanden-ID"])     # Dialogbox anzeigen
+age    = vp_info["age"]
 gender = vp_info["gender"]
-vp_id = vp_info["vp_id"]
-print(age, gender, vp_id)
+vp_id  = vp_info["vp_id"]
 
 
 # Outputordner definieren
@@ -42,6 +31,7 @@ if not os.path.exists(output_path):
     os.makedirs(output_path)
 
 file_path = os.path.join(output_path, f'vp_{vp_id}_find-human.csv')
+
 
 # dict für behav_Daten
 behav_data = {'vp_id' : [],
@@ -52,16 +42,18 @@ behav_data = {'vp_id' : [],
                 'correct_key' : [],
                 'reaction_time' : [],
                 'target' : [],                    #zur Erfassung ob Zielreiz gezeigt wurde ja/nein
+                'key' : [],
+                'size' : [] 
                           }
 
-file_path = os.path.join(output_path, f'vp{vp_id}_find-human.csv')
-
-
-# stuff specific to our experiment
+# Fenster erstellen
 win = visual.Window(
     color='grey',
     size=[1366, 768],                      # Display anpassen, mac = 2560, 1440
-    fullscr = True)                        # kann bei Mac nun geschlossen an, mac = true
+    fullscr = True                         # kann bei Mac nun geschlossen an, mac = true
+    )                        
+
+win.mouseVisible = False
 
 
 # Instruktionen definieren
@@ -74,8 +66,8 @@ event.waitKeys(maxWait=30.0, keyList=["space"])
 
 instruct_stim = visual.TextStim(win) 
 instruct_stim.setText(
-"Das Experimenten besteht aus 8 Blöcken mit 45 Trials. \n\n" \
-"Drücken Sie bitte die Taste (a) wenn ein Menschengesicht da ist und (l) wenn kein Menschengesicht da ist. \n\n" \
+"Das Experimenten besteht aus 8 Blöcken mit 45 trials. \n\n" \
+"Drücken Sie bitte die Taste [A] wenn ein Menschengesicht da ist und [L] wenn kein Menschengesicht da ist. \n\n" \
 "Legen Sie nun bitte die Finger auf die entsprechenden Tasten. \n\n"\
 "Drücken Sie die Leertaste für [weiter].")
 instruct_stim.draw()
@@ -85,13 +77,14 @@ event.waitKeys(maxWait=30.0, keyList=["space"])
 instruct_stim_2 = visual.TextStim(win) 
 instruct_stim_2.setText("Zunächst starten wir mit einem kurzen Training. \n\n " \
 "Nach erfolgreichem Training startet das eigentliche Experiment. \n\n" \
-"Drücken Sie die Leertaste, um das Experiment zu starten \n\n" \
+"Drücken Sie die Leertaste, um das Experiment zu starten. \n\n" \
 "Viel Erfolg!")   
 instruct_stim_2.draw()
 win.flip() 
 event.waitKeys(maxWait=30.0, keyList=["space"])   
 
 
+# Funktion für Experiment erstellen
 def show_display(blocks = 8, trials = 45, dict_for_data = None):
      # Seitenverhältnis Fenster
     aspect_ratio = win.size[0] / win.size[1]              # Anpassung: damit Bilder nicht in Breite gezogen werden
@@ -107,21 +100,17 @@ def show_display(blocks = 8, trials = 45, dict_for_data = None):
 
     # loop für Trials erstellen
     for m in range(blocks):
-        
-        
-
         block_text = visual.TextStim(win, text = f"Block {m+1} \n\n" \
-                                     "Drücke [a] für Mensch und [l] für kein Mensch \n\n" \
-                                        "Drücke die Leertaste, um weiter zu machen")
+                                     "Drücken Sie [A] für Mensch und [L] für kein Mensch. \n\n" \
+                                        "Drücken Sie die Leertaste, um fortzufahren.")
         block_text.draw()
         win.flip()
         event.waitKeys(keyList =["space"])
+
         for n in range(trials):
 
             # Erstellen einer Liste mit Positionen
             pos_list = []
-
-
             n_row   = 6
             n_col   = 6
             spacing =  .18                                 # Abstand zwischen Elementen
@@ -133,96 +122,104 @@ def show_display(blocks = 8, trials = 45, dict_for_data = None):
 
 
             # Größe der Displays + zufällig generieren
-
-            display_size = [8, 16, 36]
-
-            size = random.choice(display_size)
+            display_sizes = [8, 16, 36]
+            size = random.choice(display_sizes)
 
 
             # Inter Trial Intervall (ITI)
-
             iti = visual.TextStim(win, height = 0.3)         
             iti.setText("")
             iti.draw()
             win.flip()
-            core.wait(.5)
+            event.waitKeys(maxWait = .5, keyList = None)             # Es soll kein Tastendruck aufgezeichnet werden
 
-            # Fixatioskreuz anzeigen
-            fix_cross = visual.TextStim(win, height = 0.3)         # Fixationskreuz erstellen
+
+            # Fixationskreuz anzeigen
+            fix_cross = visual.TextStim(win, height = 0.3)         
             fix_cross.setText("+")
             fix_cross.draw()
             win.flip()
-            core.wait(1.)
+            event.waitKeys(maxWait = 1., keyList = None)
             
             
-
-            # Zeichnen der Rechtecke
-
+            # Zeichnen der Bilder
             for i in range(size-1):
 
                 # Ablenker zufällig auswählen
                 flanker = random.choice(img_primate)                #Liste anpassen (alle Bilder brauchen .jpeg)
-                target_list = random.choice(["human", "primate"]) #im dict festhalten, aus welcher Liste Target
-                if target_list == "human":
-                    target = random.choice(img_human)
-                elif target_list == "primate":
-                    target = random.choice(img_primate)
-                
-
                 pos = random.choice(pos_list)
-                print(pos)
                 pos_list.remove(pos)
                 img_stim = visual.ImageStim(win, image = flanker, size=[rect_width, rect_height],
-                                pos = pos)                 #default rectangle plotten
+                                pos = pos)                 
                 img_stim.draw()
 
-            
-
-
-            pos = random.choice(pos_list)                  #graue fenster als Platz für Traget, Target fehltnoch
+            target_list = random.choice(["human", "primate"])   #im dict festhalten, aus welcher Liste Target
+            if target_list == "human":
+                target = random.choice(img_human)
+            elif target_list == "primate":
+                target = random.choice(img_primate)
+            pos = random.choice(pos_list)                 
             pos_list.remove(pos)
             img_stim = visual.ImageStim(win, image = target, size=[rect_width, rect_height],
                                 pos = pos)
             img_stim.draw()
+
             win.flip()
 
-            
-            reaction_times = {}
+            reaction_times = {}             # ?
            
+
             # Startzeit messen
             start_time = time.time()
 
+
             # auf Antwort warten und Experiment abbrechen
             while True:
-                keys = event.getKeys(keyList=["a", "l", "space", "escape"])
+                keys = event.getKeys(keyList=["a", "l", "escape"])
                 if keys:
                     if 'escape' in keys:
-                        win.close()
-                        core.quit()
+                        text_stim = visual.TextStim(win)
+                        text_stim.setText("Wollen Sie das Experiment beenden?\n\nja [j]   nein [n]\n\n Bestätigen Sie Ihre Eingabe mit der Leertaste")
+                        text_stim.draw()
+                        win.flip()
+                        event.clearEvents()
+                        response = event.waitKeys(keyList = ["j", "n"])
+                        if response[0] == "j":
+                            df = pd.DataFrame.from_dict(behav_data)  # gesammelte Daten in einen DataFrame umwandeln
+                            df.to_csv(file_path, sep=",", index=False, header=True)  # Daten als CSV speichern
+                            print(f"Zwischengespeicherte Daten bis Block {m+1}, Trial {n+1}")
+                            win.close()
+                            core.quit()
+                        elif response[0] == "n":
+                            event.clearEvents()
+                            continue
                     else:
                         response = keys[0]
                         break
-
 
             # Endzeit messen
             if response:
                 end_time = time.time()
                 reaction_time = round(end_time - start_time, 4)  # Reaktionszeit berechnen
 
+
             # Reaktionszeit speichern
                 reaction_times[f'block_{m}_trial_{n}'] = {
                 'response': response[0],
                 'reaction_time': reaction_time
             }
-                print(f"Response: {response}, Reaction Time: {reaction_time:.4f} seconds") #4 Nachkommastellen
+
 
             # correct key ermitteln
-
-            correct_key = 1 if (response == ["a"] and target == "human") or (response == ["l"] and target == "primate") else 0
+            # correct_key = 1 if (response == ["a"] and target == "human") or (response == ["l"] and target == "primate") else 0
+            if response[0] == "a" and target_list == "human":
+                correct_key = True
+            elif response[0] == "l" and target_list == "primate":
+                correct_key = True
+            else:
+                correct_key = False
 
             # Dictionary befüllen
-            
-
             if dict_for_data:
                 dict_for_data["vp_id"].append(vp_id)
                 dict_for_data["age"].append(age)
@@ -232,43 +229,68 @@ def show_display(blocks = 8, trials = 45, dict_for_data = None):
                 dict_for_data["correct_key"].append(correct_key)
                 dict_for_data["reaction_time"].append(reaction_time)
                 dict_for_data["target"].append(target_list)
+                dict_for_data["key"].append(response[0])
+                dict_for_data["size"].append(size)
 
 
+                # Ende der Funktion
 
 # Training
-
-training_answer = "y"
-
-while training_answer == "y":
-    show_display(blocks=1, trials=1)    # ändern: 5
-    training_stim = visual.TextStim(win)
-    training_stim.setText("Möchtest du das Training wiederholen? \n\n "
-                      "Ja [y]      Nein [n]")
-    training_stim.draw()
-    win.flip()
-    training_answer = event.waitKeys(keyList=["y", "n"])[0]
-    print(f"Training answer: {training_answer}")
-
-
+training_dict = {'vp_id' : [],
+                'age' : [],
+                'gender' : [],
+                'block' : [],
+                'trial' : [],
+                'correct_key' : [],
+                'reaction_time' : [],
+                'target' : [],                    #zur Erfassung ob Zielreiz gezeigt wurde ja/nein
+                'key' : [],
+                'size' : [] 
+                          }
+training_answer = 0
+while training_answer <= 8:
+    training_answer = 0
+    show_display(blocks = 1, trials = 10, dict_for_data = training_dict)
+    for i in training_dict["correct_key"]:
+        if i == True:
+            training_answer = training_answer + 1
+    if training_answer <= 8:
+        text_stim = visual.TextStim(win)
+        text_stim.setText("Hier ist ein weiterer Übungsblock. \n\n" \
+                    "Drücken Sie die Leertaste, um fortzufahren.")
+        text_stim.draw()
+        win.flip()
+        event.waitKeys(keyList = ["space"])
 
 # Experiment durchführen
 text_stim = visual.TextStim(win)
-text_stim.setText("Jetzt beginnt das Experiment  \n\n" \
-                    "Drücke die Leertaste, um zu beginnen")
+text_stim.setText("Jetzt beginnt das Experiment.  \n\n" \
+                    "Drücken Sie die Leertaste, um zu beginnen.")
 text_stim.draw()
 win.flip()
 event.waitKeys(keyList = ["space"])
 show_display(blocks = 8, trials = 45, dict_for_data = behav_data)
 
 
+# Abschlussdisplay
+text_stim = visual.TextStim(win)
+text_stim.setText("Das Experiment ist beendet. \n\n  Vielen Dank für Ihre Teilnahme!")
+text_stim.draw()
+win.flip()
+event.waitKeys(keyList = ["space"])
+
+
 win.close()
 
 
+# befülltes dict in Konsole ausgeben
 for key in behav_data:
     print(key, ":", behav_data[key])
 
 
 # Dataframe speichern
-
-df = pd.DataFrame.from_dict(behav_data)                       # dict in pandas Dataframe umwandeln
-df.to_csv(file_path, sep = ",", index=False, header=True)
+try:
+    df = pd.DataFrame.from_dict(behav_data)                       # dict in pandas Dataframe umwandeln
+    df.to_csv(file_path, sep = ",", index=False, header=True)
+except:
+    print(f"Fehler beim Speichern der Datei {file_path}")
